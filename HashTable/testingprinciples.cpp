0f71a1096ee6f2e345b2303655c5c3a9e35d8c60
U-BSD\419260 <419260@SHS-HYBF6X3.beaverton.k12.or.us>
2025-02-05 10:13:06 -0800

    Welp that's not supposed to happen

--- HashTable/testingprinciples.cpp
+++ HashTable/testingprinciples.cpp
@@ -17,7 +17,9 @@ class Student{
   }
   Student(char* newFirstname, char* newLastname, int newID, float newGPA){
     strncpy(Firstname,newFirstname,10);
+    Firstname[10]='\0';
     strncpy(Lastname,newLastname,10);
+    Lastname[10]='\0';
     ID = newID;
     GPA = newGPA;
   }
@@ -68,6 +70,8 @@ void prepArray(Node **& studarray, int sizeofarray);
 void listStudents(Node* head);
 void readOutArray(Node** studarray, int sizeofarray);
 void getStringFromInput(char* inpstring);
+void killStudentPreamble(Node**& studarray, int sizeofarray);
+void killStudent(Node* current, int ID);
 
 int main(){
   srand(time(0));
@@ -80,13 +84,13 @@ int main(){
   cout<<"==========================="<<endl;
   cout<<"Welcome to:"<<endl;
   cout<<"  _____         _____           _____ _____       __  __      ________ "<<endl;
-  cout<<" / ___ \        \   /           \   / \   /      /  \/  \     \  ____/ "<<endl;
-  cout<<" | | /__\        | |             | |   | |      / /\__/\ \     | |     "<<endl;
+  cout<<" / ___ \\        \\   /           \\   / \\   /      /  \\/  \\     \\  ____/ "<<endl;
+  cout<<" | | /__\\        | |             | |   | |      / /\\__/\\ \\     | |     "<<endl;
   cout<<" | |___          | |             | |___| |      | |    | |     | |_/|  "<<endl;
-  cout<<" \____ \         | |             | _____ |      | |    | |     |  _ |  "<<endl;
-  cout<<"____ | |         | |             | |   | |      | |    | |     | | \|  "<<endl;
-  cout<<"\  /_| |         | |___          | |   | |      | |    | |     | |___  "<<endl;
-  cout<<" \_____/ tudent /______\ ister, /___\ /___\ ash/___\  /___\ap /______\ dition."<<endl;
+  cout<<" \\____ \\         | |             | _____ |      | |    | |     |  _ |  "<<endl;
+  cout<<"____ | |         | |             | |   | |      | |    | |     | | \\|  "<<endl;
+  cout<<"\\  /_| |         | |___          | |   | |      | |    | |     | |___  "<<endl;
+  cout<<" \\_____/ tudent /______\\ ister, /___\\ /___\\ ash/___\\  /___\\ap /______\\ dition."<<endl;
    bool notQuit=true;
   cout<<"(Please note this program is incapable of saving data, so don't actually use it for managing students)"<<endl;
   char inpstring[10];
@@ -106,16 +110,16 @@ int main(){
     }else if (strcmp(inpstring,"ADDRANDOM")==0){
       addRandomPreamble(IDiteration,studarray,sizeofarray,needsreset);
     }else if (strcmp(inpstring,"DELETE")==0){
-      //killStudent(head);
+      killStudentPreamble(studarray,sizeofarray);
     }else if (strcmp(inpstring,"PRINT")==0){
-      ReadOutArray(studarray,sizeofarray);
+      readOutArray(studarray,sizeofarray);
     }else if (strcmp(inpstring,"QUIT")==0){
       notQuit=false;
       //no command needed, just quit the loop.
     }else{
       cout<<"Invalid Command."<<endl;
     }
-    if(needsreset){
+    while(needsreset){
       rehash(studarray,sizeofarray,needsreset);
       resizes++;
     }
@@ -156,13 +160,12 @@ int blend(Student* blendy, int cap){
   }
   strncpy(fullname,blendy->Firstname,strlen(blendy->Firstname));
   strcat(fullname,blendy->Lastname);
-
+  fullname[21]='\0';
   unsigned long sum=0;
   for(int i=0; i<strlen(fullname); i++){
     sum=(i*sum*7489)+fullname[i];
   }
-
-  delete fullname;
+  delete[] fullname;
   return ((sum)%cap);
 }
 
@@ -234,18 +237,29 @@ void addStudent(Node**& studarray, int sizeofarray, int& newID, bool& needsreset
 }
 
 void blendAddChild(Node**& putchildin, int sizeofarray, Student* kid, bool& needsreset){
+  cout<<"Hi"<<endl;
   Student* copykid = new Student(kid->Firstname,kid->Lastname,kid->ID,kid->GPA);
   copykid->Firstname[10]='\0';
+  cout<<"there"<<endl;
   Node* copybox = new Node(copykid);
   copykid->Lastname[10]='\0';
+  cout<<"buddy"<<endl;
+  cout<<sizeofarray<<endl;
+  cout<<kid<<endl;
+  cout<<blend(kid,sizeofarray)<<endl;
+  cout<<putchildin[blend(kid,sizeofarray)]<<endl;
   linearAdd(putchildin[blend(kid,sizeofarray)],putchildin[blend(kid,sizeofarray)], copybox);
+  cout<<"chum"<<endl;
   if(needsreset==false){
     needsreset = NeedRehash(putchildin[blend(kid,sizeofarray)]);
   }
+  cout<<"pal"<<endl;
 }
 
 void linearAdd(Node*& head, Node* current, Node* addme){
+  cout<<"My favorite food is"<<endl;
   if (current->getStudent()==nullptr){
+    cout<<"Salt"<<endl;
     delete current;
     current=new Node(addme->getStudent());
     current->setNext(nullptr);
@@ -253,14 +267,18 @@ void linearAdd(Node*& head, Node* current, Node* addme){
     addme =new Node(nullptr);
     delete addme;
   }else if (head->getStudent()->ID > addme->getStudent()->ID){
+    cout<<"Sodium"<<endl;
     head = addme;
     head->setNext(current);
   }else if (current->getNext()==nullptr){
+    cout<<"Stibnite"<<endl;
     current->setNext(addme);
   }else if ((current->getStudent()->ID < addme->getStudent()->ID)&&(current->getNext()->getStudent()->ID > addme->getStudent()->ID)){
+    cout<<"Sauteed Beef"<<endl;
     addme->setNext(current->getNext());
     current->setNext(addme);
   }else{
+    cout<<"The concept of Steel"<<endl;
     linearAdd(head, current->getNext(), addme);
   }
 }
@@ -283,26 +301,22 @@ Student* Randomkid(int& ID){
   for(int i=0; i<11;i++){
     fname[i]='\0';
   }
-  char* nuchar = new char((rand()%26)+65);
-  strncpy(fname, nuchar,1);
+  char nuchar = (rand()%26)+65;
+  strncpy(fname, &nuchar,1);
   for(int i=0; i<amt;i++){
-    delete nuchar;
-    nuchar = new char((rand()%26)+97);
-    strncat(fname, nuchar,1);
+    nuchar = (rand()%26)+97;
+    strncat(fname, &nuchar,1);
   }
-  
   amt=(rand()%5+5);
   char* lname = new char[11];
   for(int i=0; i<11;i++){
     lname[i]='\0';
   }
-  delete nuchar;
-  nuchar = new char((rand()%26)+65);
-  strncpy(lname, nuchar,1);
+  nuchar = (rand()%26)+65;
+  strncpy(lname, &nuchar,1);
   for(int i=0; i<amt;i++){
-    delete nuchar;
-    nuchar = new char((rand()%26)+97);
-    strncat(lname, nuchar,1);
+    nuchar = (rand()%26)+97;
+    strncat(lname, &nuchar,1);
   }
   Student* randokid = new Student(fname,lname,ID++,(((float)(rand()%400))/100));
   delete[] fname;
@@ -320,10 +334,10 @@ void addRandomKids(int howmany, int& ID, Node**&studarray, int sizeofarray, bool
 
 void addRandomPreamble(int& ID, Node**&studarray, int sizeofarray, bool& needsreset){
   bool acin=false;
-  int numbtoad;
+  int numbtoadd;
   while (acin==false){
-    cout<<"Please give me a number of students."<<endl;
-    cin>>ID;
+    cout<<"Please give a number of students to add"<<endl;
+    cin>>numbtoadd;
     if(cin.fail()){
       cout<<"ERROR! please try again."<<endl;
       cin.clear();
@@ -332,7 +346,9 @@ void addRandomPreamble(int& ID, Node**&studarray, int sizeofarray, bool& needsre
       acin=true;
     }
   }
-  
+  cout<<"Adding students..."<<endl;
+  addRandomKids(numbtoadd,ID,studarray,sizeofarray,needsreset);
+  cout<<"Complete! Added "<<numbtoadd<<" Students."<<endl;
 }
 
 void clearDown(Node* head){
@@ -369,5 +385,87 @@ void readOutArray(Node**studarray,int sizeofarray){
     cout<<endl<<"Box Number: "<<i<<endl;
     cout<<"Contents: "<<endl;
     listStudents(studarray[i]);
+  }
+}
+
+//Student Deleter Preamble.
+void killStudentPreamble(Node**& studarray, int sizeofarray){
+  readOutArray(studarray, sizeofarray);
+  char* inpstring = new char[11];
+  Student* emptychild = new Student();
+  cin.clear();
+  cin.ignore(100000,'\n');
+  cout<<"Please provide student's first name.(Case and character Sensitive)"<<endl;
+  getStringFromInput(inpstring);
+  strncpy(emptychild->Firstname,inpstring,11);
+  cout<<"Please provide student's last name.(Case and character Sensitive)"<<endl;
+  getStringFromInput(inpstring);
+  strncpy(emptychild->Lastname,inpstring,11);
+  int box = blend(emptychild,sizeofarray);
+  cout<<"Student is in box number "<<box<<endl;
+  listStudents(studarray[box]);
+  delete[] inpstring;
+  delete emptychild;
+  if (studarray[box]->getStudent()==nullptr){
+    cout<<"Box empty. Canceling DELETE..."<<endl;
+    return;
+  }
+  cout<<"Due to certain factors, multiple students with the same name can exist."<<endl;
+  cout<<"Please provide ID of student you wish to delete. if some mistake has been made, enter a negative number to cancel"<<endl;
+  bool acin=false;
+  int index;
+  while (acin==false){
+    cout<<"Please Enter Student ID (Serial Integer): "<<endl;
+    cin>>index;
+    if(cin.fail()){
+      cout<<"I think you did something wrong. please try again."<<endl;
+      cin.clear();
+      cin.ignore(100000,'\n');
+    }else{
+      acin=true;
+    }
+  }
+  if(index<0){
+    return;
+  }
+  if(studarray[box]->getStudent()->ID==index){
+    if(studarray[box]->getNext()!=nullptr){
+      Student* buffer = studarray[box]->getStudent();
+      Node* afterhead = studarray[box]->getNext();
+      delete studarray[box];
+      studarray[box] = new Node(afterhead->getStudent());
+      studarray[box]->setNext(afterhead->getNext());
+      afterhead = new Node(buffer);
+      delete afterhead;
+    }else{
+      delete studarray[box]->getStudent();
+      studarray[box] = new Node(nullptr);
+    }
+    return;
+  }
+  killStudent(studarray[box],index);
+  
+}
+
+//Main student deletion code. Recycled from old project.
+void killStudent(Node* current, int ID){
+  if(current->getNext()==nullptr){
+    //if we're in the head
+    delete current->getStudent();
+    current = new Node(nullptr);
+    return;
+  }else if(current->getNext()->getStudent()->ID==ID){
+    //take the node out of the loop, then delete it.
+    Node* killbuffer = current->getNext();
+    current->setNext(current->getNext()->getNext());
+    delete killbuffer;
+    return;
+  }else{
+    //if we can't move on, we should just leave.
+    if (current->getNext()->getNext()==nullptr){
+      cout<<"ERROR: No student with that ID exists!"<<endl;
+    }else {
+      killStudent(current->getNext(),ID);
+    }
   }
 }
