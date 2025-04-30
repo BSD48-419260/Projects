229be0d497b094a6217d4b1ee5749a38f72b157c
U-BSD\419260 <419260@SHS-HYBF6X3.beaverton.k12.or.us>
2025-04-30 09:56:48 -0700

    consolidating code

---
+++ Cybersec/Xor/hex64.cpp
@@ -0,0 +1,24 @@
+void getStringFromInput(char* inpstring){
+  char bufferarray [101];
+  //make sure it works
+  bool acin=false;
+  for(int i=0;i<101;i++){
+    bufferarray[i]='\0';
+  }
+  while(acin==false){
+    cout<<"100 characters or less, please."<<endl;
+    cin.getline(bufferarray, sizeof(bufferarray),'\n');
+    //being robust.
+    if(cin.fail()){
+      cout<<"I think you did something wrong. Please try again."<<endl;
+      cin.clear();
+      cin.ignore(100000,'\n');
+    }else{
+      acin=true;
+    }
+  }
+  strncpy(inpstring, bufferarray, 100);
+  inpstring[100]='\0';
+  cout<<endl;
+  return;
+}
